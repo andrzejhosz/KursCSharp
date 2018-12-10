4ad40452975b6cdc94433dda482c6b20eec8a00e
Andrzej Hosz <andrzej.hosz@gmail.com>
2018-12-10 18:54:57 +0100

    pierwsze wgranie

---
+++ Lekcja0.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kurs_C_Sharp
+{
+    class Lekcja0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lekcja1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lekcja1
+{
+    class Lekcja1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
